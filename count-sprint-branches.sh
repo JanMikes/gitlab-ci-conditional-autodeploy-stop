3086f4331f1beb6972d07b7f1f5a889801c9771e
Jan Mikeš <j.mikes@me.com>
2021-09-28 00:28:47 +0200

    Debug statements fixing

--- count-sprint-branches.sh
+++ count-sprint-branches.sh
@@ -2,6 +2,6 @@
 
 branches_prefix="^sprint-"
 
-branches_response=$(curl -H "Authorization: Bearer $CI_JOB_TOKEN" https://gitlab.com/api/v4/projects/"$CI_PROJECT_ID"/repository/branches?search="$branches_prefix")
+branches_response=$(curl --header "Authorization:Bearer $CI_JOB_TOKEN" "https://gitlab.com/api/v4/projects/$CI_PROJECT_ID/repository/branches?search=$branches_prefix")
 
 echo $branches_response | jq 'length'
