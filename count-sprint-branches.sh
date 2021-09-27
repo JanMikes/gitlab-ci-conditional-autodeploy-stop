4f1d54e5c7ccaf8cb850988dda70595a8c09fb1f
Jan Mikeš <j.mikes@me.com>
2021-09-28 00:04:10 +0200

    Fix missing dollar string

--- count-sprint-branches.sh
+++ count-sprint-branches.sh
@@ -4,4 +4,4 @@ branches_prefix="^sprint-"
 
 branches_response=$(curl -H "Authorization: Bearer $CI_JOB_TOKEN" https://gitlab.com/api/v4/projects/"$CI_PROJECT_ID"/repository/branches?search="$branches_prefix")
 
-echo branches_response | jq 'length'
+echo $branches_response | jq 'length'
