f2ce7e9e640f0eb2d5cf5147af22cec12a62324c
Jan Mikeš <j.mikes@me.com>
2021-09-27 23:52:43 +0200

    Using sprint branches in condition

---
+++ count-sprint-branches.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+branches_prefix="^sprint-"
+
+branches_response=$(curl -H "Authorization: Bearer $CI_JOB_TOKEN" https://gitlab.com/api/v4/projects/"$CI_PROJECT_ID"/repository/branches?search="$branches_prefix")
+
+echo branches_response | jq 'length'
