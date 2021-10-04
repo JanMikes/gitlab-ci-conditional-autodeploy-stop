35595e22c20da9382295ffa73f0489412fa418c2
Jan Mikeš <j.mikes@me.com>
2021-10-04 13:21:46 +0200

    Deploy automatically if is latest sprint branch

---
+++ get-latest-branch-name.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+branches_prefix="^sprint-"
+
+branches_response=$(curl --header "Authorization:Bearer $PERSONAL_ACCESS_TOKEN" "https://gitlab.com/api/v4/projects/$CI_PROJECT_ID/repository/branches?search=$branches_prefix")
+
+echo $branches_response | jq 'max_by(.name) | .name'
