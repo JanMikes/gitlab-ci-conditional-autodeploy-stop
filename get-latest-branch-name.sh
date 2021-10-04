ce7226e0a44a00ee1aebadd11b09567e6f189bb2
Jan Mikeš <j.mikes@me.com>
2021-10-04 13:41:48 +0200

    Stripping " from variable value

--- get-latest-branch-name.sh
+++ get-latest-branch-name.sh
@@ -4,4 +4,8 @@ branches_prefix="^sprint-"
 
 branches_response=$(curl --header "Authorization:Bearer $PERSONAL_ACCESS_TOKEN" "https://gitlab.com/api/v4/projects/$CI_PROJECT_ID/repository/branches?search=$branches_prefix")
 
-echo $branches_response | jq 'max_by(.name) | .name'
+echo $branches_response | jq 'max_by(.name) | .name' | tr -d '"'
+
+# see https://stackoverflow.com/questions/9733338/shell-script-remove-first-and-last-quote-from-a-variable
+# stripping " from variable is needed - see https://gitlab.com/janmikes/gitlab-ci-conditional-autodeploy-stop/-/jobs/1646519281
+# echo "$opt" | tr -d '"'
